2f8bd32c8c5b0bf9a793bf94feb030eea1cf53c2
Licínio Mendes <licinioamendes@gmail.com>
2021-02-21 11:40:02 +0000

    more projects different frameworks

---
+++ src/Presentation.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using my_lib;
+
+namespace Presentation.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = new Class1(Console.WriteLine);
+            x.Log();
+        }
+    }
+}
--- src/my-lib/Class1.cs
+++ src/my-lib/Class1.cs
@@ -4,5 +4,13 @@ namespace my_lib
 {
     public class Class1
     {
+        private Action<string> _writer;
+
+        public Class1(Action<string> writer)
+        {
+            _writer = writer;
+        }
+
+        public void Log() => _writer($"Logging {DateTime.Now:s}");
     }
 }
